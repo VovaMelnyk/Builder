e21ec9b6359b0437d130b4bcb4ab7c06dec12e14
Vlad Lutsenko <lutsenko.vladyslav@gmail.com>
2020-08-01 14:38:40 +0300

    rb-59--first

--- src/Components/Preview/Preview.js
+++ src/Components/Preview/Preview.js
@@ -1,7 +1,12 @@
 import React from "react";
 import styles from "./Preview.module.css";
+import PreviewT1 from "./RB-59/PreviewT1";
 const Preview = () => {
-  return <div></div>;
+  return (
+    <div>
+      <PreviewT1 />
+    </div>
+  );
 };
 
 export default Preview;
