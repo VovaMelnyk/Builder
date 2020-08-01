3812bca76196d26249896ab5479f1ff590210d6a
KirillKulakow <kirillkulakow31@gmail.com>
2020-08-01 19:16:50 +0300

    add redux and formulas in component

--- src/Components/Education/EducationItem.js
+++ src/Components/Education/EducationItem.js
@@ -1,105 +1,83 @@
-import React, { useState } from "react"
+import React from "react"
 import style from "./Education.module.css";
 import withToggle from "../../hoc/withToggle";
 import DatePicker from "react-datepicker";
 import moment from "moment";
 
 
-const EducationItem =({el, open, toggle}) => {
-
-    const initialState = {
-        school: "Best school ever",
-        degree: "master",
-        start: "",
-        end: "",
-      };
-
-    const [form, setForm] = useState(initialState)
-
-    const handleChange = (e) => {
-        const {name, value} = e.target
-        console.log(name, value);
-        setForm({...form, [name]: value})
-    }
-
-      const handleChangeEnd = (date1) => {
-        setForm({ ...form, end: date1 });
+const EducationItem =({school, index, degree, start, end, open, toggle, handleRemoveClick}) => {
+  const remove = (index) => {
+    handleRemoveClick(index);
   };
+return (  
+<div>        
+  <div className={style.degree}>
+    <div className={style.degreeSchool}>
+    <h2 className={style.schoolLabel}>{`${!!school ? school : 'School'} ${!!degree ? degree : 'degree'}`}</h2>
+    <div className={style.degreeEdits}>
+      <div className={style.degreeEditIcon}></div>
+        <p className={style.degreeEditsHover}>Edit</p>
+        <div className={style.degreeDeleteIcon}></div>
+        <p className={style.degreeEditsHover} onClick={remove}>Delete</p>
+        <div
+        className={
+          !open
+            ? style.degreeTreeangle
+            : style.degreeTreeangleRotate
+        }
+        onClick={toggle}
+        ></div>
+      </div>
+    </div>
 
-  const handleChangeStart = (date1) => {
-    setForm({ ...form, start: date1 });
-  };
+  <div className={style.degreeDates}>
+    <p className={style.degreeText}>Start</p>
+    <p className={style.Date}>{`${moment(start).format("l")}`}</p>
+    <p className={style.degreeText}>End</p>
+    <p className={style.Date}>{`${moment(end).format("l")}`}</p>
+  </div>
+</div> 
 
+{open && (
+<div className={style.fillIn}>
+  <input
+    type="text"
+    placeholder="School"
+    name="school"
+    className={style.inputSpace}
+    value={school}
+    // onChange={handleChange}
+  />
+  <input
+    type="text"
+    placeholder="Degree"
+    name="degree"
+    className={style.inputSpace}
+    value={degree}
+    // onChange={handleChange}
+  />
 
-  console.log(form);
+  <div className={style.fillInDate}>
+    {/* <input type="date" placeholder="Start date" name="startDate" /> */}
+    <DatePicker
+      selected={start}
+      // onChange={handleChangeStart}
+      className={style.inputDate}
+      name="start"
+    />
+    <DatePicker
+      selected={end}
+      // onChange={handleChangeEnd}
+      className={style.inputDate}
+      name="end"
+    />
+    {/* <input type="date" placeholder="End date" name="endDate" className={style.inputDate}/> */}
+  </div>
+</div>
+)}
 
-            return (  
-            <div>        
-        <div className={style.degree}>
-            <div className={style.degreeSchool}>
-              <h2 className={style.schoolLabel}>School degree</h2>
-              <div className={style.degreeEdits}>
-                <div className={style.degreeEditIcon}></div>
-                <p className={style.degreeEditsHover}>Edit</p>
-                <div className={style.degreeDeleteIcon}></div>
-                <p className={style.degreeEditsHover}>Delete</p>
-                <div
-                  className={
-                    !open
-                      ? style.degreeTreeangle
-                      : style.degreeTreeangleRotate
-                  }
-                  onClick={toggle}
-                ></div>
-              </div>
-            </div>
-
-            <div className={style.degreeDates}>
-              <p className={style.degreeText}>Start</p>
-              <p className={style.Date}>{`${moment(el.start).format("l")}`}</p>
-              <p className={style.degreeText}>End</p>
-              <p className={style.Date}>{`${moment(el.end).format("l")}`}</p>
-            </div>
-          </div> 
-          
-          {open && (
-            <div className={style.fillIn}>
-                <input
-                  type="text"
-                  placeholder="School"
-                  name="school"
-                  className={style.inputSpace}
-                  onChange={handleChange}
-                />
-                <input
-                  type="text"
-                  placeholder="Degree"
-                  name="degree"
-                  className={style.inputSpace}
-                  onChange={handleChange}
-                />
-
-                <div className={style.fillInDate}>
-                  {/* <input type="date" placeholder="Start date" name="startDate" /> */}
-                  <DatePicker
-                    selected={el.start}
-                    onChange={handleChangeStart}
-                    className={style.inputDate}
-                    name="start"
-                  />
-                  <DatePicker
-                    selected={el.end}
-                    onChange={handleChangeEnd}
-                    className={style.inputDate}
-                    name="end"
-                  />
-                  {/* <input type="date" placeholder="End date" name="endDate" className={style.inputDate}/> */}
-                </div>
-              </div>
-          )}
-          
-          </div>
-          )
+</div>
+)
 }
 
 export default withToggle(EducationItem);
--- src/redux/reducers/educations.js
+++ src/redux/reducers/educations.js
@@ -1,3 +1,5 @@
+import { ADD_EDUCATION, SET_EDUCATION, DELETE_EDUCATION } from '../actions/educations';
+
 const initialState = [
   {
     school: "Best school ever",
@@ -9,6 +11,14 @@ const initialState = [
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case ADD_EDUCATION:
+      return [...state, action.payload]
+    case SET_EDUCATION:
+      return
+    case DELETE_EDUCATION:
+      const newStateDelete = state.slice();
+      newStateDelete.splice(action.payload, 1);
+      return newStateDelete;
     default:
       return state;
   }
