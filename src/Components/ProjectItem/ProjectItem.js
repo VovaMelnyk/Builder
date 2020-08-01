204ac56cbbfd29b17c3d9906eb26972bb3378f06
KirillKulakow <kirillkulakow31@gmail.com>
2020-08-01 17:29:50 +0300

    btn Add, remove, edit project and add redux

--- src/Components/ProjectItem/ProjectItem.js
+++ src/Components/ProjectItem/ProjectItem.js
@@ -1,12 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./ProjectItem.module.css";
+import { setProject } from "../../redux/projects/action";
+import { useDispatch } from 'react-redux';
 
-const ProjectItem = () => {
-    const [title, setTitle] = useState('Project Title');
+const ProjectItem = ({removeProject, index, company, description, title}) => {
+    const [titleProject, setTitleProject] = useState(title);
+    const [companyProject, setCompanyProject] = useState(company);
+    const [descriptionProject, setDescriptionProject] = useState(description);
     const [hiddenForm, setHiddenForm] = useState(false);
 
-    const titleChange = (e) => {
-        setTitle(e.target.value)
+    const dispatch = useDispatch();
+
+    const changeTitle = (e) => {
+        setTitleProject(e.target.value);
+        dispatch(setProject(index, {projectTitle: e.target.value, company: companyProject, description: descriptionProject}))
+    };
+    const changeCompany = (e) => {
+        setCompanyProject(e.target.value);
+        dispatch(setProject(index, {projectTitle: titleProject, company: e.target.value, description: descriptionProject }))
+    };
+    const changeDescription = (e) => {
+        setDescriptionProject(e.target.value);
+        dispatch(setProject(index, {projectTitle: titleProject, company: companyProject, description: e.target.value}))
+    };
+
+    const remove = () => {
+        removeProject(index);
     };
 
     const toggleHide = () => {
@@ -17,24 +36,24 @@ const ProjectItem = () => {
         if(hiddenForm){
             setHiddenForm(!hiddenForm); 
         }
-    }
+    };
 
     return (
         <div className={styles.project_item}>
             <div className={styles.header}>
-                <p className={styles.header__title}>{title}</p>
+                <p className={styles.header__title}>{titleProject}</p>
                 <div className={styles.header__btn_container}>
                     <button className={`${styles.header__btn} ${styles.header__btn_pen}`} onClick={toggleHideEdit}>Edit</button>
-                    <button className={`${styles.header__btn} ${styles.header__btn_close}`}>Delete</button>
+                    <button className={`${styles.header__btn} ${styles.header__btn_close}`} onClick={remove}>Delete</button>
                     <button className={`${styles.header__btn} ${hiddenForm ? styles.header__btn_hide_down : styles.header__btn_hide_up}`} onClick={toggleHide}></button>
                 </div>
             </div>
             <form className={hiddenForm ? styles.header__form_hide : styles.header__form}>
                 <div className={styles.header_form_container}>
-                    <input className={styles.form__title} type="text" placeholder="Project Title" onChange={titleChange}></input>
-                    <input className={styles.form__company} type="text" placeholder="Company"></input>
+                    <input className={styles.form__title} type="text" placeholder="Project Title" value={titleProject} onChange={changeTitle}></input>
+                    <input className={styles.form__company} type="text" placeholder="Company" value={companyProject} onChange={changeCompany}></input>
                 </div>
-                <textarea className={styles.form__description} type="text" placeholder="Description"></textarea>
+                <textarea className={styles.form__description} type="text" placeholder="Description" value={descriptionProject} onChange={changeDescription}></textarea>
             </form>
         </div>
     );
