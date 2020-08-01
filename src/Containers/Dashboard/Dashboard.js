7dc486af55e42668ab46d2c2af97a5fd0774a09e
IvanPustovit <pustovit.for@gmail.com>
2020-08-01 16:21:13 +0300

    Dashboard_add

--- src/Containers/Dashboard/Dashboard.js
+++ src/Containers/Dashboard/Dashboard.js
@@ -2,7 +2,12 @@ import React from "react";
 import styles from "./Dashboard.module.css";
 
 const Dashboard = () => {
-  return <div></div>;
+  return (
+    <div className={styles.dashboard}>
+      <h1 className={styles.created}>Created Resume</h1>
+      <button className={styles.createButton}>Create New</button>
+    </div>
+  );
 };
 
 export default Dashboard;
