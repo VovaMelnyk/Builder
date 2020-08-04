5d5640fe984188206a3cc45deea67703392cb317
NazarKinash <nazarkynash16@gmail.com>
2020-08-04 19:20:15 +0300

    fixTheme

--- src/Components/Education/Education.js
+++ src/Components/Education/Education.js
@@ -2,52 +2,59 @@ import React from "react";
 import style from "./Education.module.css";
 import EducationItem from "./EducationItem";
 import { addEducation, deleteEducation } from "../../redux/actions/educations";
-import { connect, useDispatch } from "react-redux";
+import { connect, useDispatch, useSelector } from "react-redux";
 
 import "react-datepicker/dist/react-datepicker.css";
 import AddButton from "../AddButton/AddButton";
 
 const Education = ({ educations }) => {
-  const dispatch = useDispatch();
+	const dispatch = useDispatch();
+	const theme = useSelector((state) => state.theme);
 
-  const handleAddClick = () => {
-    dispatch(
-      addEducation({
-        school: "",
-        degree: "",
-        start: Date.now(),
-        end: Date.now()
-      })
-    );
-  };
+	const handleAddClick = () => {
+		dispatch(
+			addEducation({
+				school: "",
+				degree: "",
+				start: Date.now(),
+				end: Date.now(),
+			})
+		);
+	};
 
-  const handleRemoveClick = index => {
-    dispatch(deleteEducation(index));
-  };
+	const handleRemoveClick = (index) => {
+		dispatch(deleteEducation(index));
+	};
 
-  return (
-    <div className={style.wraper}>
-      <h1 className={style.title}>Education</h1>
-      <div className={style.itemsWrapper}>
-        {educations.map((el, index) => (
-          <EducationItem
-            key={index}
-            school={el.school}
-            index={index}
-            degree={el.degree}
-            start={el.start}
-            end={el.end}
-            handleRemoveClick={handleRemoveClick}
-          />
-        ))}
-      </div>
-      <AddButton onClick={handleAddClick} title={"Education"} />
-    </div>
-  );
+	return (
+		<div className={style.wraper}>
+			<h1
+				className={
+					theme === "dark" ? `${style.title} ${style.titleDark}` : style.title
+				}
+			>
+				Education
+			</h1>
+			<div className={style.itemsWrapper}>
+				{educations.map((el, index) => (
+					<EducationItem
+						key={index}
+						school={el.school}
+						index={index}
+						degree={el.degree}
+						start={el.start}
+						end={el.end}
+						handleRemoveClick={handleRemoveClick}
+					/>
+				))}
+			</div>
+			<AddButton onClick={handleAddClick} title={"Education"} />
+		</div>
+	);
 };
 
-const mapStateToProps = state => ({
-  educations: state.resume.educations
+const mapStateToProps = (state) => ({
+	educations: state.resume.educations,
 });
 
 export default connect(mapStateToProps)(Education);
--- src/Components/Education/EducationItem.js
+++ src/Components/Education/EducationItem.js
@@ -3,141 +3,204 @@ import style from "./Education.module.css";
 import withToggle from "../../hoc/withToggle";
 import DatePicker from "react-datepicker";
 import moment from "moment";
-import { connect, useDispatch } from "react-redux";
+import { connect, useSelector, useDispatch } from "react-redux";
 import { setEducation } from "../../redux/actions/educations";
 
 const EducationItem = ({
-  school,
-  index,
-  degree,
-  start,
-  end,
-  open,
-  toggle,
-  handleRemoveClick
+	school,
+	index,
+	degree,
+	start,
+	end,
+	open,
+	toggle,
+	handleRemoveClick,
 }) => {
-  const remove = index => {
-    handleRemoveClick(index);
-  };
+	const remove = (index) => {
+		handleRemoveClick(index);
+	};
 
-  const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-  const handleSchool = e => {
-    dispatch(
-      setEducation(index, {
-        school: e.target.value,
-        degree: degree,
-        start: start,
-        end: end
-      })
-    );
-  };
+	const handleSchool = (e) => {
+		dispatch(
+			setEducation(index, {
+				school: e.target.value,
+				degree: degree,
+				start: start,
+				end: end,
+			})
+		);
+	};
 
-  const handleDegree = e => {
-    dispatch(
-      setEducation(index, {
-        school: school,
-        degree: e.target.value,
-        start: start,
-        end: end
-      })
-    );
-  };
+	const handleDegree = (e) => {
+		dispatch(
+			setEducation(index, {
+				school: school,
+				degree: e.target.value,
+				start: start,
+				end: end,
+			})
+		);
+	};
 
-  const handleStart = data => {
-    dispatch(
-      setEducation(index, {
-        school: school,
-        degree: degree,
-        start: data,
-        end: end
-      })
-    );
-  };
+	const handleStart = (data) => {
+		dispatch(
+			setEducation(index, {
+				school: school,
+				degree: degree,
+				start: data,
+				end: end,
+			})
+		);
+	};
 
-  const handleEnd = data => {
-    dispatch(
-      setEducation(index, {
-        school: school,
-        degree: degree,
-        start: start,
-        end: data
-      })
-    );
-  };
+	const handleEnd = (data) => {
+		dispatch(
+			setEducation(index, {
+				school: school,
+				degree: degree,
+				start: start,
+				end: data,
+			})
+		);
+	};
 
-  return (
-    <div>
-      <div className={style.degree}>
-        <div className={style.degreeSchool}>
-          <h2 className={style.schoolLabel}>{`${!!school ? school : "School"} ${
-            !!degree ? degree : "degree"
-          }`}</h2>
-          <div className={style.degreeEdits}>
-            <button onClick={toggle} className={`${style.degreeEditsHover} ${style.degreeEditIcon}`}>Edit</button>
-            <button onClick={remove} className={`${style.degreeEditsHover} ${style.degreeDeleteIcon}`}>Delete</button>
-            <button
-              className={`${style.degreeEditsHover} ${!!open ? style.degreeTreeangle : style.degreeTreeangleRotate}`
-              }
-              onClick={toggle}
-            ></button>
-          </div>
-        </div>
+	const theme = useSelector((state) => state.theme);
 
-        <div className={style.degreeDates}>
-          <p className={style.degreeText}>Start</p>
-          <p className={style.Date}>
-            {start ? `${moment(start).format("l")}` : "mm/yy"}
-          </p>
-          <p className={style.degreeText}>End</p>
-          <p className={style.Date}>
-            {end ? `${moment(end).format("l")}` : "mm/yy"}
-          </p>
-        </div>
-      </div>
+	return (
+		<div>
+			<div
+				className={
+					theme === "dark"
+						? `${style.degree} ${style.degreeDark}`
+						: style.degree
+				}
+			>
+				<div className={style.degreeSchool}>
+					<h2
+						className={
+							theme === "dark"
+								? `${style.schoolLabel} ${style.schoolLabelDark}`
+								: style.schoolLabel
+						}
+					>{`${!!school ? school : "School"} ${
+						!!degree ? degree : "degree"
+					}`}</h2>
+					<div className={style.degreeEdits}>
+						<button
+							onClick={toggle}
+							className={
+								theme === "dark"
+									? `${style.degreeEditsHover} ${style.degreeEditIcon} ${style.degreeEditsHoverDark} ${style.degreeEditIconDark}`
+									: `${style.degreeEditsHover} ${style.degreeEditIcon}`
+							}
+						>
+							Edit
+						</button>
+						<button
+							onClick={remove}
+							className={
+								theme === "dark"
+									? `${style.degreeEditsHover} ${style.degreeDeleteIcon} ${style.degreeEditsHoverDark} ${style.degreeDeleteIconDark}`
+									: `${style.degreeEditsHover} ${style.degreeDeleteIcon}`
+							}
+						>
+							Delete
+						</button>
+						<button
+							className={`${style.degreeEditsHover} ${
+								!!open
+									? style.degreeTreeangle
+									: theme === "dark"
+									? `${style.degreeTreeangleRotate} ${style.degreeTreeangleRotateDark}`
+									: style.degreeTreeangleRotate
+							}`}
+							onClick={toggle}
+						></button>
+					</div>
+				</div>
 
-      {open && (
-        <div className={style.fillIn}>
-          <input
-            type="text"
-            placeholder="School"
-            name="school"
-            className={style.inputSpace}
-            // value={school}
-            onChange={handleSchool}
-            // onChange={handleChange}
-          />
-          <input
-            type="text"
-            placeholder="Degree"
-            name="degree"
-            className={style.inputSpace}
-            // value={degree}
-            onChange={handleDegree}
-          />
+				<div className={style.degreeDates}>
+					<p
+						className={
+							theme === "dark" ? style.degreeTextDark : style.degreeText
+						}
+					>
+						Start
+					</p>
+					<p
+						className={
+							theme === "dark" ? style.degreeDateDark : style.degreeDate
+						}
+					>
+						{start ? `${moment(start).format("l")}` : "mm/yy"}
+					</p>
+					<p
+						className={
+							theme === "dark" ? style.degreeTextDark : style.degreeText
+						}
+					>
+						End
+					</p>
+					<p
+						className={
+							theme === "dark" ? style.degreeDateDark : style.degreeDate
+						}
+					>
+						{end ? `${moment(end).format("l")}` : "mm/yy"}
+					</p>
+				</div>
+			</div>
 
-          <div className={style.fillInDate}>
-            {/* <input type="date" placeholder="Start date" name="startDate" /> */}
-            <DatePicker
-              selected={moment(start)._d}
-              onChange={handleStart}
-              className={style.inputDate}
-              dateFormat="MM/yyyy"
-              showMonthYearPicker
-            />
-            <DatePicker
-              selected={moment(end)._d}
-              onChange={handleEnd}
-              className={style.inputDate}
-              dateFormat="MM/yyyy"
-              showMonthYearPicker
-            />
-            {/* <input type="date" placeholder="End date" name="endDate" className={style.inputDate}/> */}
-          </div>
-        </div>
-      )}
-    </div>
-  );
+			{open && (
+				<div
+					className={
+						theme === "dark"
+							? `${style.fillIn} ${style.fillInDark}`
+							: style.fillIn
+					}
+				>
+					<input
+						type="text"
+						placeholder="School"
+						name="school"
+						className={style.inputSpace}
+						// value={school}
+						onChange={handleSchool}
+						// onChange={handleChange}
+					/>
+					<input
+						type="text"
+						placeholder="Degree"
+						name="degree"
+						className={style.inputSpace}
+						// value={degree}
+						onChange={handleDegree}
+					/>
+
+					<div className={style.fillInDate}>
+						{/* <input type="date" placeholder="Start date" name="startDate" /> */}
+						<DatePicker
+							selected={moment(start)._d}
+							onChange={handleStart}
+							className={style.inputDate}
+							dateFormat="MM/yyyy"
+							showMonthYearPicker
+						/>
+						<DatePicker
+							selected={moment(end)._d}
+							onChange={handleEnd}
+							className={style.inputDate}
+							dateFormat="MM/yyyy"
+							showMonthYearPicker
+						/>
+						{/* <input type="date" placeholder="End date" name="endDate" className={style.inputDate}/> */}
+					</div>
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default withToggle(EducationItem);
